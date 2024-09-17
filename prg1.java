4c7c4b64edfcf4513e33c3889070365a75874e26
Akhand Pratap Singh <singhakhandpratap55@gmail.com>
2024-09-17 12:13:36 +0530

    Create prg1.java

---
+++ prg1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class RotateMatrix{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the matrix: ");
+        int n = scanner.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        rotateMatrix(matrix, n);
+    }
+    public static void rotateMatrix(int[][] matrix, int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
